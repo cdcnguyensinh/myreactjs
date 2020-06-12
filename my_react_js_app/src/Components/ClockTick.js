bc2b242d846cb0960a7d5df952bbc4d869e5f3a2
Sinh Nguyen <sinh.nguyen@nois.vn>
2020-06-12 09:49:18 +0700

    update state for clocktick.js

--- my_react_js_app/src/Components/ClockTick.js
+++ my_react_js_app/src/Components/ClockTick.js
@@ -5,47 +5,63 @@ import React, { Component } from 'react';
 // import * as serviceWorker from './serviceWorker';
 
 class Clock extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {date: new Date()};
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+      current: ""
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        ...this.state,
+        current: new Date().toISOString()
+      })
+    }, 1000);
+  }
 
 
 
   render() {
-    const TickTime1 = () => {
-      return <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    }
+    // const TickTime1 = () => {
+    //   return <div>
+    //     <h1>Hello, world!</h1>
+    //     <h2>It's {this.state.date.toLocaleDateString()}.</h2>
+    //   </div>
+    // }
 
-    const Tick2 = () => {
-      return <ClockTick date={new Date()} />
-    }
+    // const Tick2 = () => {
+    //   return <ClockTick date={new Date()} />
+    // }
+
+
 
     return (
       <div>
-        <TickTime1 />
-        <Tick2 />
+        {/* <TickTime1 />
+        <Tick2 /> */}
+        <ClockTick3 current={this.state.current} />
       </div>
     )
   }
 }
-
-function GetTick()
-{
-  return (
-    <div>welcome</div>
-  );
-}
-
 
 function ClockTick(props) {
   return (
     <div>
       <h1>This is tick time!</h1>
       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function ClockTick3(props) {
+  return (
+    <div>
+      <h1>This is tick time 3!</h1>
+      <h2>It is {props.current}.</h2>
     </div>
   );
 }
